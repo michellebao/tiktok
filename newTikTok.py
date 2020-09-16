44585a51b023799261730a91f4816d4452decf32
michellebao <42559259+michellebao@users.noreply.github.com>
2020-09-16 15:13:32 -0400

    Create newTikTok.py

---
+++ newTikTok.py
@@ -0,0 +1,241 @@
+import pprint
+import argparse
+from TikTokApi import TikTokApi
+import inspect
+import json
+
+api = TikTokApi(debug=True, executablePath="/usr/lib/chromium-browser/chromium-browser")
+pp = pprint.PrettyPrinter()
+
+#### Aggregate Functions ####
+
+def getTrendingTikToks(n:int = 10, printOutput:bool = False) -> dict: # returns info on n trending tiktoks
+  trendingTiktoks = api.trending(count=n)
+  lst = []
+  for tiktok in trendingTiktoks:
+      resp = processTikTokObject(tiktok)
+      lst.append(resp)
+  if printOutput:
+      pp.pprint(lst)
+  return lst
+
+def getTrendingHashtags(n:int = 10, printOutput:bool = False) -> dict: # returns info on hashtags/challenges shown on side of trending page on desktop (up to 10)
+  trendingHashtags = api.discoverHashtags()
+  lst = []
+  for i in range(min(n, 10)):
+      hashtag = trendingHashtags[i].get('cardItem', {})
+      id = hashtag.get('id')
+      resp = {}
+      resp['id'] = id
+      resp['title'] = hashtag.get('title')
+      resp['desc'] = hashtag.get('description')
+      resp['views'] = hashtag.get('extraInfo', {}).get('views')
+      lst.append(resp)
+  if printOutput:
+      pp.pprint(lst)
+  return lst
+
+def getTrendingMusic(n:int = 10, printOutput:bool = False) -> dict: # returns info on music shown on side of trending page on desktop
+  trendingMusic = api.discoverMusic()
+  lst = []
+  resp = {}
+  for i in range(min(n, 10)):
+      music = trendingMusic[i].get('cardItem', {})
+      resp = processMusicObject(music)
+      lst.append(resp)
+  if printOutput:
+      pp.pprint(lst)
+  return lst
+
+#### Functions for Individual Objects ####
+
+def getHashtagInfo(hashtags, printOutput:bool = False) -> dict: # returns info for specific hashtag (exclude # symbol from string)
+  lst = []
+  for hashtag in hashtags:
+    resp = {}
+    obj = api.getHashtagObject(hashtag)
+    if (obj.get('statusCode') != 0):
+        print("Error getting object for hashtag: ", hashtag)
+        continue
+    hashtagInfo = obj.get('challengeInfo', {})
+    resp['id'] = hashtagInfo.get('challenge', {}).get('id') 
+    resp['title'] = hashtagInfo.get('challenge', {}).get('title') 
+    resp['desc'] = hashtagInfo.get('challenge', {}).get('desc') 
+    resp['videoCount'] = hashtagInfo.get('stats', {}).get('videoCount')
+    resp['viewCount'] = hashtagInfo.get('stats', {}).get('viewCount')
+    lst.append(resp)
+  if printOutput:
+    pp.pprint(lst)
+  return lst
+
+def getUserInfo(usernames: list, printOutput:bool = False) -> dict: # returns info for specific user account
+  lst = []
+  for username in usernames:
+    obj = api.getUser(username)
+    resp = {}
+    if (obj.get('statusCode') != 0):
+        print("Error getting object for username: ", username)
+        continue
+    usernameInfo = obj.get('userInfo', {})
+    resp['id'] = usernameInfo.get('user', {}).get('id')
+    resp['username'] = usernameInfo.get('user', {}).get('uniqueId')
+    resp['verified'] = usernameInfo.get('user', {}).get('verified')
+    resp['followingCount'] = usernameInfo.get('stats', {}).get('followingCount')
+    resp['followerCount'] = usernameInfo.get('stats', {}).get('followerCount')
+    resp['heartCount'] = usernameInfo.get('stats', {}).get('heartCount')
+    resp['videoCount'] = usernameInfo.get('stats', {}).get('videoCount')
+    resp['diggCount'] = usernameInfo.get('stats', {}).get('diggCount')
+    lst.append(resp)
+    if printOutput:
+        pp.pprint(lst)
+  return lst
+
+def getUserLikedByUsername(username: str, n:int = 10, printOutput:bool = False) -> list: # returns list of given user's liked tiktoks (returns 0 if private list)
+  userId = usernameToUserId(username)
+  obj = api.getUser(username)
+  userInfo = obj.get('userInfo', {})
+  likedList = api.userLiked(userId, userInfo.get('user', {}).get('secUid'), count=n)
+  lst = []
+  for tiktok in likedList:
+      resp = processTikTokObject(tiktok)
+      lst.append(resp)
+  if printOutput:
+      pp.pprint(lst)
+  return lst
+
+def getSuggestedUsers(usernames:str, n:int = 10, crawl:bool = False, printOutput:bool = False) -> list: # returns list of suggested users given an account. 
+  # if crawl is True, will return suggested users for given username's suggested users
+  lst = []
+  for username in usernames:
+    userId = usernameToUserId(username)
+    suggested = api.getSuggestedUsersbyIDCrawler(count = n, startingId = userId)# if crawl else api.getSuggestedUsersbyID(count = count, userId = userId)
+    for user in suggested:
+      # print(user)
+      userN = user.get('subTitle')
+      # print(userN)
+      if userN:
+        userN = userN.replace('@', '')
+        lst += getUserInfo([userN], printOutput)
+  return lst
+
+def getSuggestedHashtags(usernames:str, n:int = 10, crawl:bool = False, printOutput:bool = False) -> list: # returns list of suggested hashtags given an account
+  # if crawl is True, will return suggested hashtags for given username's suggested users
+  lst = []
+  for username in usernames:
+    userId = usernameToUserId(username)
+    suggested = api.getSuggestedHashtagsbyIDCrawler(count = n, startingId = userId) #if crawl else api.getSuggestedHashtagsbyID(count = count, userId = userId)
+    for hashtag in suggested:
+      hashStr = hashtag.get('title')
+      if hashStr:
+        hashStr = hashStr.replace('#', '')
+        lst += getHashtagInfo([hashStr], printOutput)
+  return lst
+
+def getSuggestedMusic(usernames:str, n:int = 10, crawl:bool = False, printOutput:bool = False) -> list: # returns list of suggested music given an account
+  # if crawl is True, will return suggested music for given username's suggested users
+  lst = []
+  for username in usernames:
+    userId = usernameToUserId(username)
+    suggested = api.getSuggestedMusicIDCrawler(count = n, startingId = userId) #if crawl else api.getSuggestedMusicbyID(count = count, userId = userId)
+    for music in suggested:
+        resp = processMusicObject(music)
+        lst.append(resp)
+  if printOutput:
+    pp.pprint(lst)
+  return lst
+
+#### Util Functions ####
+
+def processTikTokObject(tiktok:dict) -> dict:
+  id = tiktok['id']
+  resp = {}
+  resp['id'] = id
+  resp['desc'] = tiktok.get('desc')
+  resp['createTime'] = tiktok.get('createTime')
+  resp['playAddr'] = tiktok.get('video', {}).get('playAddr')
+  resp['username'] = tiktok.get('author', {}).get('uniqueId')
+  resp['diggCount'] = tiktok.get('stats', {}).get('diggCount')
+  resp['shareCount'] = tiktok.get('stats', {}).get('shareCount')
+  resp['commentCount'] = tiktok.get('stats', {}).get('commentCount')
+  resp['playCount'] = tiktok.get('stats', {}).get('playCount')
+  return resp
+
+def processMusicObject(music:dict) -> dict:
+  id = music.get('id')
+  resp = {}
+  resp['id'] = id
+  resp['title'] = music.get('title')
+  resp['desc'] = music.get('description')
+  resp['playUrl'] = music.get('extraInfo', {}).get('playUrl')
+  resp['videoCount'] = music.get('extraInfo', {}).get('posts')
+  return resp
+
+def usernameToUserId(username: str) -> str:
+  obj = api.getUser(username)
+  userInfo = obj.get('userInfo', {})
+  userId = userInfo.get('user', {}).get('id')
+  return userId
+
+def main():
+# optional arguments:
+# -h, --help            show this help message and exit
+# --function {getTrendingTikToks,getTrendingHashtags,getTrendingMusic,getHashtagInfo,getUserInfo,getUserLikedByUsername,getSuggestedUsers,getSuggestedHashtags,getSuggestedMusic}
+#                       Function name
+# --n N                 n count
+# --printOutput PRINTOUTPUT
+#                       Print output
+# --hashtag HASHTAG     Hashtag string
+# --username USERNAME   Username
+# --crawl CRAWL         Crawl
+  FUNCTION_MAP = {
+    'getTrendingTikToks': getTrendingTikToks,
+    'getTrendingHashtags': getTrendingHashtags,
+    'getTrendingMusic': getTrendingMusic,
+    'getHashtagInfo': getHashtagInfo,
+    'getUserInfo': getUserInfo,
+    'getUserLikedByUsername': getUserLikedByUsername,
+    'getSuggestedUsers': getSuggestedUsers,
+    'getSuggestedHashtags': getSuggestedHashtags,
+    'getSuggestedMusic': getSuggestedMusic
+  }
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--function", choices = FUNCTION_MAP.keys(), required=True)
+  parser.add_argument("--n", type=int, required=False)
+  parser.add_argument("--printOutput", type=str, required=False)
+  parser.add_argument("--hashtag", nargs='+', required=False)
+  parser.add_argument("--username", nargs='+', required=False)
+  parser.add_argument("--outFile", type=str, required=False)
+
+  default_n = 10
+  default_print = False
+
+  args = parser.parse_args()
+  func = FUNCTION_MAP[args.function]
+  out = None
+  if args.function == 'getTrendingTikToks' or args.function == 'getTrendingHashtags' or args.function == 'getTrendingMusic':
+    out = func(args.n or default_n, args.printOutput or default_print)
+  if args.function == 'getHashtagInfo':
+    if not args.hashtag:
+      print("No hashtag given")
+      return
+    out = func(args.hashtag, args.printOutput or default_print)
+  if args.function == 'getUserInfo':
+    if not args.username:
+      print("No username given")
+      return
+    out = func(args.username, args.printOutput or default_print)
+  if args.function == 'getUserLikedByUsername':
+    if not args.username:
+      print("No username given")
+      return
+    out = func(args.username, args.n or default_n, args.printOutput or default_print)
+  if args.function == 'getSuggestedUsers' or args.function == 'getSuggestedHashtags' or args.function == 'getSuggestedMusic':
+    if not args.username:
+      print("No username given")
+      return
+    out = func(args.username, args.n or default_n, args.printOutput or default_print)
+  
+  with open(args.outFile, 'w') as outfile:
+    json.dump(out, outfile)
+
+main()
